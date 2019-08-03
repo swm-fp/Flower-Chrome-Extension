b0e5a2a44294c36ff53457790c7a0e7bcb324fe5
Chae Eun Lee <nuguziii@naver.com>
2019-08-04 00:48:33 +0900

    add remove/create/update bookmark function

--- public/app/background_bookmark.js
+++ public/app/background_bookmark.js
@@ -1,3 +1,4 @@
+
 getEntireTree('1');
 let nodeList = [];
 
@@ -25,7 +26,7 @@ function addNodes(arr){
 function getEntireTree(id){
     chrome.bookmarks.getSubTree(id, function(tree){
         addNodes(tree[0].children);
-        //console.log(nodeList);
+        //console.log(tree);
         let totalInfo = new Object();
         totalInfo.link = nodeList;
 
@@ -34,23 +35,65 @@ function getEntireTree(id){
     });
 }
 
+/*
+1. remove: 
+{"id" : id}
+
+2. create:
+{"id":,"parentID":,"title":,"url":,isFolder:}
+
+
+3. update:
+{"id":, "parentID":, "title":, "url":, "isFolder":}
+*/
+
 chrome.bookmarks.onCreated.addListener(function (id, bookmark) {
-    console.log("action:create// id: "+id+"\n");
-    console.log(bookmark);
-    //getEntireTree('1');
+    let data = new Object();
+    data.id = bookmark.id;
+    data.parentID = bookmark.parentId;
+    data.title = bookmark.title;
+    data.url = bookmark.url || null;
+    data.isFolder = (data.url == null);
+
+    let jsonInfo = JSON.stringify(data);
+    console.log("[create] "+jsonInfo);
+    //createAPI("data");
 });
 
 chrome.bookmarks.onChanged.addListener(function (id, bookmark) {
-    console.log("action:change// id: "+id+"\n");
-    console.log(bookmark);
+    chrome.bookmarks.get(id, function (nodes){
+        let data = new Object();
+        node = nodes[0];
+        data.id = id;
+        data.parentID = node.parentId;
+        data.title = node.title;
+        data.url = node.url;
+        data.isFolder = (data.url == null);
+
+        let jsonInfo = JSON.stringify(data);
+        console.log("[changed] "+jsonInfo);
+    });
 });
 
 chrome.bookmarks.onRemoved.addListener(function (id, bookmark) {
-    console.log("action:remove// id: "+id+"\n");
-    console.log(bookmark);
+    let data = new Object();
+    data.id = id;
+    
+    let jsonInfo = JSON.stringify(data);
+    console.log("[remove] "+jsonInfo);
 });
 
 chrome.bookmarks.onMoved.addListener(function (id, bookmark) {
-    console.log("action:move// id: "+id+"\n");
-    console.log(bookmark);
+    chrome.bookmarks.get(id, function (nodes){
+        let data = new Object();
+        node = nodes[0];
+        data.id = id;
+        data.parentID = node.parentId;
+        data.title = node.title;
+        data.url = node.url;
+        data.isFolder = (data.url == null);
+
+        let jsonInfo = JSON.stringify(data);
+        console.log("[moved] "+jsonInfo);
+    });
 });

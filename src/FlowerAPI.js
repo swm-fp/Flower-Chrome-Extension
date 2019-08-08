fafff5598e3fefadbc02ff05dcb27430516da30d
Chae Eun Lee <nuguziii@naver.com>
2019-08-08 23:32:21 +0900

    북마크 필요없는 함수 삭제 및 형식 수정

--- src/FlowerAPI.js
+++ src/FlowerAPI.js
@@ -1,5 +1,6 @@
 /* global chrome */
 import axios from 'axios'
+import bookmark from "./bookmark";
 
 function getUserInfo(){
   return chrome.storage.local.get(["token","id"]);
@@ -28,35 +29,12 @@ async function sendRequest(url,method,data={}){
   return response;
 }
 
-function makeRequest_bookmark(token) {
-  return {
-    /*
-      params: { // query string
-        title: 'NEXT JS'
-      },
-      */
-    headers: {
-      // 요청 헤더
-      bookmarks: token
-    }
-    //timeout: 3000 // 1초 이내에 응답이 오지 않으면 에러로 간주
-  };
-}
-
-async function getBookmarkTest() {
-  let response = await chrome.runtime.sendMessage(
-    {
-      greeting: "hello"
-    }
-  );
-
-  console.log(`message from background: ${response}`);
-
+async function getBookmarkTest(){
   let url = "https://lh5z9ce6yc.execute-api.ap-northeast-2.amazonaws.com/test/bookmark";
-  let data = JSON.parse(response);
+  let data = await bookmark.getEntireTree('1');
   let httpResponse = await sendRequest(url,"post",data);
   console.log(httpResponse.data);
-  }
+}
 
 async function getSampleNodes(){
   let url = "https://2i0zlhluc1.execute-api.ap-northeast-2.amazonaws.com/beta";

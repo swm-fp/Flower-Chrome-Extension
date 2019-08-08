cc72529f5607ab9176dcaab5fdbc4eda6a078b62
subinium <subinium@gmail.com>
2019-08-08 12:26:18 +0900

    레이아웃 재추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LoginButton from './LoginButton'
-import TestButton from './TestButton'
-import BMButton from './BMButton';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoginButton from "./LoginButton";
+import TestButton from "./TestButton";
+import GRAPH from "./GraphGen";
+import BMButton from "./BMButton";
+import "bootstrap-4-grid/css/grid.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Flower Demo Version</h1>
-        </header>
-        <p className="App-intro">
-          새로운 북마크 서비스를 사용해보세요.
-        </p>
-        <LoginButton/>
-        <TestButton/>
-        <BMButton/>
+      <div className="h-100 bootstrap-wrapper">
+        <div className="h-100 app-container container-fluid">
+          <div className="row">
+            <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+              <h1>FLOWER</h1>
+            </div>
+            <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 buttons-right">
+              <BMButton />
+              <GRAPH />
+              <LoginButton />
+              <TestButton />
+            </div>
+          </div>
+          <div className="row h-100">
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+              <h4>NAV</h4>
+              <p>Option1</p>
+              <p>Option1</p>
+              <p>Option1</p>
+              <p>Option1</p>
+              <p>Option1</p>
+              <p>Option1</p>
+            </div>
+            <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xl-10">
+              <div className="row h-100">
+                <div
+                  id="graph"
+                  className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"
+                >
+                  <h4>Graph</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+              <h4>Options</h4>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/GraphGen.js
+++ src/GraphGen.js
@@ -1,20 +1,39 @@
 /* global d3 */
-import React, { Component } from 'react';
-import { getSampleNodes } from './FlowerAPI'
+import React, { Component } from "react";
+import { getSampleNodes } from "./FlowerAPI";
+import "./Graph.css";
 
+async function Tree() {
+  let result = await getSampleNodes();
+  console.log(result);
+  let dataSet = [];
+  for (let i = 0; i < result.length; i++) {
+    dataSet.push(parseInt(result[i].nodeId, 10) * 10);
+  }
+  let svg = d3
+    .select("#graph")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 800);
 
-function Graph_Read(){
-	//let result = await  getSampleNodes();
-  //console.log(result);
-  d3.selectAll("p").style("color", "blue");
+  let circles = svg
+    .selectAll("circle")
+    .data(dataSet)
+    .enter()
+    .append("circle");
 
+  circles
+    .attr("cx", (d, i) => {
+      return i * 50 + 25;
+    })
+    .attr("cy", 800 / 2)
+    .attr("r", d => {
+      return d;
+    });
 }
 
 export default class GRAPH extends Component {
   render() {
-    return (
-      <div onClick={Graph_Read}>helo</div>
-
-	);
+    return <button onClick={Tree}>Tree</button>;
   }
 }

211863115cccc24b390bb5d42194cfc59dfb8f57
현우 변 <bhw1994@gmail.com>
2019-08-08 14:54:51 +0900

    Nodes 함수 추가

---
+++ resources/nodes/handler.js
@@ -0,0 +1,77 @@
+'use strict';
+let uniqid = require('uniqid');
+let AWS = require('aws-sdk');
+let documentClient = new AWS.DynamoDB.DocumentClient();
+
+
+
+module.exports.read = async event => {
+  let pathParameters = event.pathParameters;
+  let projectId = pathParameters.projectId;
+  
+  let params = {
+    TableName : 'Nodes',
+    FilterExpression : 'projectId = :projectId',
+    ExpressionAttributeValues : {':projectId' : projectId}
+  };
+  
+  let result = await documentClient.scan(params).promise();
+  
+  return {
+    statusCode: 200,
+    body:JSON.stringify(
+      {
+        result : result
+      }
+      )
+      
+    };
+  };
+  
+  
+  
+  function convertToBatchWriteFormat(data,projectId){
+    let items = [];
+    
+    for(let i in data){
+      let node = data[i];
+      node["id"] = uniqid();
+      node["projectId"] = projectId;
+      
+      let item = {
+        PutRequest: {
+          Item: node
+        }
+      };
+      items.push(item);
+    }
+    
+    
+    let params = {
+      RequestItems: {
+        'Nodes' : items
+      }
+    };
+    
+    return params;
+  }
+  
+  module.exports.create= async event => {
+    let pathParameters = event.pathParameters;
+    let projectId = pathParameters.projectId;
+    
+    let body = JSON.parse(event.body);
+    let data = body["data"];
+    let params = convertToBatchWriteFormat(data,projectId);
+    let result = await documentClient.batchWrite(params).promise();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          result : result
+        }
+        )
+        
+      };
+    };

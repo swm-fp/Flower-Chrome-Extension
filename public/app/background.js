32a43768de929bc007df89b24278478e459a4bfd
현우 변 <bhw1994@gmail.com>
2019-08-10 12:09:18 +0900

    메모 작성 기능 추가

--- public/app/background.js
+++ public/app/background.js
@@ -6,3 +6,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
    });
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+   chrome.contextMenus.create({
+       //id: key,
+       title: "add Memo",
+       type: 'normal',
+       contexts: ['page'],
+       onclick:function(){
+
+
+
+           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+               chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+                 console.log(response);
+               });
+             });
+       }
+     });
+ });

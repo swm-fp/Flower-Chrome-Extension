df9b319e09e4bf4a2b50a9b92950f47a288c37c6
Chae Eun Lee <nuguziii@naver.com>
2019-08-22 17:37:55 +0900

    create getTag function and button

--- src/popup.js
+++ src/popup.js
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/popup.css";
+import FlowerAPI from "./FlowerAPI";
+
+async function Tags() {
+  let node = 
+  {
+    title : "플라워 북마크 서비스로 하는 효율적인 북마크 관리!",
+    url : "www",
+    memo : undefined,
+    highlight : undefined
+  }
+  let data = await FlowerAPI.getTags(node);
+  console.log(data);
+}
 
 class Popup extends Component {
   render() {
@@ -10,6 +23,7 @@ class Popup extends Component {
       <div class="container">
         <h1>Sidepannel</h1>
         <div className="Memo" />
+        <Button onClick={Tags}>북마크 추가</Button>
       </div>
     );
   }

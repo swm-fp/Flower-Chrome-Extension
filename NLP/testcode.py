1a9413fdf79c074a5039256b7a1746b95b149c7a
Chae Eun Lee <nuguziii@naver.com>
2019-09-16 23:38:18 +0900

    file open 부분, api 가져오는 부분 수정

--- NLP/testcode.py
+++ NLP/testcode.py
@@ -1,15 +1,22 @@
 import csv
-from utils import keyword_extractor
+from utils import *
 
-with open('./list.csv', 'r', encoding='utf-8') as csvfile:
-    r = csv.DictReader(csvfile)
-    for l in r:
-        result = {}
-        result["title"], result["url"] = l["title"], l["url"]
-        result["keywords"], result["result"] = keyword_extractor(l["title"], "")
+api_list = {1:'kkma', 2:'Twitter', 3:'Hannanum', 4:'Komoran'}
 
-        with open('./data.csv', 'a', encoding='utf-8') as csvfile:
-            fieldnames = ['title', 'url', 'keywords', 'result']
-            wr = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            #wr.writeheader()
+with open('./data.csv', 'w', encoding='utf-8') as csvfile:
+    fieldnames = ['title', 'url', 'kkma', 'Twitter', 'Hannanum', 'Komoran']
+    wr = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    wr.writeheader()
+
+    with open('./list.csv', 'r', encoding='utf-8') as csvfile:
+        r = csv.DictReader(csvfile)
+        for l in r:
+            result = {}
+            result["title"], result["url"] = l["title"], l["url"]
+            result[api_list[1]], _ = api(1,l["title"])
+            result[api_list[2]], _ = api(2,l["title"])
+            result[api_list[3]], _ = api(3,l["title"])
+            result[api_list[4]], _ = api(4,l["title"])
+
+            print(result," ......finished")
             wr.writerow(result)

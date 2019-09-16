fa1d794ca84487b355eff238c9540ff81ea7497e
Chae Eun Lee <nuguziii@naver.com>
2019-09-16 23:39:07 +0900

    add data cleansing

--- NLP/utils.py
+++ NLP/utils.py
@@ -1,93 +1,65 @@
 import operator, re
-from konlpy.tag import Kkma, Twitter
+from konlpy.tag import Kkma, Twitter, Hannanum, Komoran
 from textblob import TextBlob
+from collections import Counter
 
-def compare_api(api_name, sen):
+def clean_sentence(sen):
+    sen = re.sub('[-=+,#/\?:^$.@*\"※~&%·ㆍ!』♥☆\\‘|\(\)\[\]\<\>`\'…》]', ' ', sen)
+    sen = sen.strip()
+    return sen
+
+def filter(words):
+    words = list(set(words))
+    res = []
+    for w in words:
+        if not (len(w) <= 1 or w.isdigit()):
+            res.append(w)
+    return res
+
+def deleteRepetes(words):
+    res = []
+    for i in range(len(words)):
+        inFlag = False
+        for temp in words[:i]+words[i+1:]:
+            if words[i] in temp:
+                inFlag=True
+                break
+        if not inFlag:
+            res.append(words[i])
+    return res
+
+def eng_nouns(sen):
+    res = []
+    temp = TextBlob(sen).noun_phrases
+    for r in temp:
+        if len(re.findall(u'[\u3130-\u318F\uAC00-\uD7A3]+', r))==0:
+            res.append(r)
+    return res
+
+def api(api_num, sen):
+    sen = clean_sentence(sen)
+
     module = None
-    if api_name=='kkma':
+    if api_num==1:
         module = Kkma()
-    elif api_name=='Twitter':
+    elif api_num==2:
         module = Twitter()
+    elif api_num==3:
+        module = Hannanum()
+    elif api_num==4:
+        module = Komoran()
 
-    return module.pos(sen)
+    #pos = module.pos(sen)
+    return deleteRepetes(filter(module.nouns(sen))) + eng_nouns(sen), module.pos(sen)
 
-def posResult(sen): #단어 단위로 형태소 분석 후 반환
-    kkma = Kkma()
+def keyword_extractor(title, highlight=''):
+    title_nouns = api(2, title)
 
-    #print("nouns: ", kkma.nouns(sen))
-
-    pos = kkma.pos(sen)
-    morphs = [x[0] for x in pos] #형태소
-
-    parse = sen.split(' ')
-    result = []
-    for p in parse:
-        temp = []
-        while True:
-            try:
-                target = morphs[0]
-            except:
-                break
-            print(target)
-            if target in p:
-                temp.append(pos[0])
-            elif morphs[0] not in sen:
-                pass
-            else:
-                break
-            pos = pos[1:]
-            morphs = morphs[1:]
-        result.append(temp)
-    
-    print(result)
-    return result
-
-def toWords(pos):
-    for cur in pos:
-        word_temp = ''
-        for c in cur:
-            if 'N' in c[0]:
-                word_temp += c[0]
-            pass
-
-
-def keyword_extractor(title, highlight):
-    konl = Kkma()
-    eng_title = TextBlob(re.sub("[^A-Za-z]", ",", title.strip())).noun_phrases
-    eng_highlight = TextBlob(re.sub("[^A-Za-z]", " ", highlight.strip())).noun_phrases
-    title_nouns = konl.nouns(title)
-    highlight_nouns = konl.nouns(highlight)
-
-    #line = re.sub("[^A-Za-z]", "", title.strip())
-
-    keyword_list = {i: 2 for i in title_nouns}
-    for i in highlight_nouns:
-        try:
-            keyword_list[i] +=1
-        except:
-            keyword_list[i] = 1
-
-    for i in eng_title:
-        keyword_list[i] = 2
-
-    for i in eng_highlight:
-        try:
-            keyword_list[i] +=1
-        except:
-            keyword_list[i] = 1
-
-    keyword_list = sorted(keyword_list.items(), key=operator.itemgetter(1), reverse=True)
-    
-    keywords={}
-    for i, k in enumerate(keyword_list):
-        if not k[0].isdigit():
-            keywords[str("k"+str(i))] = k[0]
-
-    return keyword_list, posResult(title)
+    return title_nouns
 
 if __name__ == "__main__":
     while True:
-        sent = input("문장입력>> ")
-        posResult(sent)
+        sen = input("문장입력>> ")
+        res = eng_nouns(sen)
+        print(res)
         print()
-    

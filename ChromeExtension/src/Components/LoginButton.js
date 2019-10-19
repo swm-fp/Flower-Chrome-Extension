342f26632ad9a4cf4486316e9f2c9c3f56f24c18
subinium <subinium@gmail.com>
2019-10-19 20:48:39 +0900

    newtab 디자인 기초 구조 변경

--- ChromeExtension/src/Components/LoginButton.js
+++ ChromeExtension/src/Components/LoginButton.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-export default class LoginButton extends Component {
-  render() {
-    return (
-      <Button>
-        <a href="https://fpsampleuser.auth.ap-northeast-2.amazoncognito.com/login?response_type=token&client_id=4tja7g1k102ahesgi70t63icq3&redirect_uri=https://fpsample.s3.ap-northeast-2.amazonaws.com/success.html">
-          login button
-        </a>
-      </Button>
-    );
-  }
+import React from "react";
+import Button from "@material-ui/core/Button";
+export default function LoginButton() {
+  return (
+    <Button variant="outlined">
+      <a href="https://fpsampleuser.auth.ap-northeast-2.amazoncognito.com/login?response_type=token&client_id=4tja7g1k102ahesgi70t63icq3&redirect_uri=https://fpsample.s3.ap-northeast-2.amazonaws.com/success.html">
+        login button
+      </a>
+    </Button>
+  );
 }
--- ChromeExtension/src/newtab.js
+++ ChromeExtension/src/newtab.js
@@ -1,57 +1,183 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import Sidebar from "react-sidebar";
+
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/newtab.css";
-import NavCustom from "./Components/NavCustom";
+import "./css/newtab.scss";
 import MainDashboard from "./Components/MainDashboard";
-import SidebarCustom from "./Components/SidebarCustom";
 
-const mql = window.matchMedia(`(min-width: 800px)`);
+import clsx from "clsx";
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  makeStyles
+} from "@material-ui/core/styles";
 
-function NewTab(props) {
-  const [sidebarDocked, setSidebarDocked] = useState(mql.matches);
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  const styles = { sidebar: { width: "180px", align: "center" } };
-  useEffect(() => {
-    mql.addListener(mediaQueryChanged);
-    return () => {
-      mql.removeListener(mediaQueryChanged);
-    };
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { teal, yellow } from "@material-ui/core/colors";
+
+import LoginButton from "./Components/LoginButton";
+
+const drawerWidth = 180;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  title: {
+    flexGrow: 1
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap"
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}));
+
+export default function NewTab() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: teal,
+      secondary: yellow
+    }
   });
 
-  const onSetSidebarOpen = open => {
-    setSidebarOpen(open);
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
   };
 
-  const mediaQueryChanged = () => {
-    setSidebarDocked(mql.matches);
-    setSidebarOpen(false);
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
   return (
-    <div className="bootstrap-wrapper">
-      <Sidebar
-        className="sidebar"
-        sidebar={<SidebarCustom />}
-        open={sidebarOpen}
-        docked={sidebarDocked}
-        onSetOpen={onSetSidebarOpen}
-        styles={styles}
-      >
-        <NavCustom />
-        <Route path="/" component={MainDashboard} />
-      </Sidebar>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              touch-it
+            </Typography>
+            <LoginButton />
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open
+            })
+          }}
+          open={open}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+        </Drawer>
+
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+
+          <MainDashboard />
+        </main>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
-ReactDOM.render(
-  <BrowserRouter>
-    <NewTab />,
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<NewTab />, document.getElementById("root"));
 registerServiceWorker();

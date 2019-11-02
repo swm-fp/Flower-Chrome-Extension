357a5070517327bec0c7dd44f2973f43bdec2010
subinium <subinium@gmail.com>
2019-11-02 13:22:41 +0900

    view를 위한 일부 요소 수정

--- ChromeExtension/src/Components/LoginButton.js
+++ ChromeExtension/src/Components/LoginButton.js
@@ -4,7 +4,7 @@ export default function LoginButton() {
   return (
     <Button variant="outlined">
       <a href="https://fpsampleuser.auth.ap-northeast-2.amazoncognito.com/login?response_type=token&client_id=4tja7g1k102ahesgi70t63icq3&redirect_uri=https://flower-chrome-extension.s3.ap-northeast-2.amazonaws.com/login_success.html">
-        login button
+        login
       </a>
     </Button>
   );

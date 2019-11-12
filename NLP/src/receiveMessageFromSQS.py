fdf7c67fc2504f71ba4a5ab911ba96e243a5da3a
Chae Eun Lee <nuguziii@naver.com>
2019-11-12 19:01:31 +0900

    sqs receive message 추가

--- NLP/src/receiveMessageFromSQS.py
+++ NLP/src/receiveMessageFromSQS.py
@@ -1,30 +1,48 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from ast import literal_eval
 
 # Create SQS client
 sqs = boto3.client('sqs', region_name='ap-northeast-2')
 
 queue_url = 'https://sqs.ap-northeast-2.amazonaws.com/616448378550/newQueue'
+dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
+table = dynamodb.Table('UrlTag')
 
-# Receive message from SQS queue
-response = sqs.receive_message(
-    QueueUrl=queue_url,
-    AttributeNames=[
-        'SentTimestamp'
-    ],
-    MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
-    VisibilityTimeout=0,
-    WaitTimeSeconds=0
-)
+while 1:
+    # Receive message from SQS queue
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=0
+    )
+    
+    try:
+        message = response['Messages'][0]
+        receipt_handle = message['ReceiptHandle']
+        body = literal_eval(message["Body"])
 
-message = response['Messages'][0]
-receipt_handle = message['ReceiptHandle']
+        print(body["url"])
 
-# Delete received message from queue
-sqs.delete_message(
-    QueueUrl=queue_url,
-    ReceiptHandle=receipt_handle
-)
-print('Received and deleted message: %s' % message)
+        response = table.put_item(
+            Item={
+                    'url': body["url"],
+                    'tags': ["sqstest"]
+                }
+        )
+
+        # Delete received message from queue
+        sqs.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
+        print('Received and deleted message: %s' % body["url"])
+    except:
+        pass
